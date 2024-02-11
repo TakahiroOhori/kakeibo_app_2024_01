5d51d6fb02a03df48f79c8749e7e12d7e0be7f6c
Takahiro Ohori <47852646+TakahiroOhori@users.noreply.github.com>
2024-02-12 00:59:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import japanese_matplotlib
+import japanize_matplotlib
 import seaborn as sns
 import datetime
 
